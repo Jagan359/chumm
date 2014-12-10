ae5bb4944eb4738f4c043dab6f3fce2e071c68c0
Jagan <jagan262gmail.com>
2014-12-10 18:07:02 +0000

    match to get post

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
    root 'customers#hmpg'
    get 'customers/index'
-   match 'customers/oauth'
+   post 'customers/oauth'
+get 'customers/oauth'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
